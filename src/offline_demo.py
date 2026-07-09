8767fb6fdf8f90d0615844a53f9fa98bc8764d92
willxxy <willhan327@outlook.com>
2026-07-09 12:26:08 -0400

    update

--- src/offline_demo.py
+++ src/offline_demo.py
@@ -1,19 +1,20 @@
 import sglang
 
-llm = sglang.Engine(model_path="qwen/qwen2.5-0.5b-instruct")
+if __name__ == "__main__":
+    llm = sglang.Engine(model_path="qwen/qwen2.5-0.5b-instruct")
 
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
+    prompts = [
+        "Hello, my name is",
+        "The president of the United States is",
+        "The capital of France is",
+        "The future of AI is",
+    ]
 
-sampling_params = {"temperature": 0.8, "top_p": 0.95}
+    sampling_params = {"temperature": 0.8, "top_p": 0.95}
 
-outputs = llm.generate(prompts, sampling_params)
+    outputs = llm.generate(prompts, sampling_params)
 
-for prompt, output in zip(prompts, outputs):
-    print(f"Prompt: {prompt}\nGenerated text: {output['text']}\n")
+    for prompt, output in zip(prompts, outputs):
+        print(f"Prompt: {prompt}\nGenerated text: {output['text']}\n")
 
-llm.shutdown()
+    llm.shutdown()
